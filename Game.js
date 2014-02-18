caa3c0d5f760b7cbe5dce79a2e5a312575c40295
Michael Pair <michael.pair1@marist.edu>
2014-02-18 10:29:14 -0500

    locale descriptions
    When the player enters a room, a description of the room is shown as
    well as where the user is

--- Game.js
+++ Game.js
@@ -13,38 +13,39 @@ var item
 var locale = new Array();
     locale[0] = new loc(0);
         locale[0].name = "Main Garage";
-        locale[0].desc = "locale0 desc";
+        locale[0].desc = "There is a car that you can go beneath, as well as doors in all directions There are racks with tires on the wall and a toolboox with a wrench sitting on top.";
         locale[0].toString();
         mite(0);
         locale[1] = new loc(1);
         locale[1].name = "Waiting Room";
-        locale[1].desc = "locale1 desc";
+        locale[1].desc = "There are lots of chairs and table for people to sit at while they wait for their car to be fixed. There is a door to the north that says \"Private\" and another door that says \"Garage\" to the east";
         locale[1].toString();
     locale[2] = new loc(2);
         locale[2].name = "Storage Room";
-        locale[2].desc = "locale2 desc";
+        locale[2].desc = "There are stacks of boxes containing car parts. A door to the west leads to the Main Office";
         locale[2].toString();
     locale[3] = new loc(3);
         locale[3].name = "Main Office";
-        locale[3].desc = "locale3 desc";
+        locale[3].desc = "A big desk sits in the middle of the room with file cabinets all over the place. There is a pen on the desk. A windowed door to the west reveals a junkyard";
         locale[3].toString();
         mite(2);
     locale[4] = new loc(4);
         locale[4].name = "Junk Yard";
-        locale[4].desc = "locale4 desc";
+        locale[4].desc = "There are stacks of crushed cars that appear to go on for miles. In the trunk of a car nearby, there's a gun.";
         locale[4].toString();
         mite(3);
     locale[5] = new loc(5);
         locale[5].name = "Smog Check Garage";
-        locale[5].desc = "locale5 desc";
+        locale[5].desc = "There is a button that says do not press, but it appears to be broken";
+        //button is not pressable yet.
         locale[5].toString();
     locale[6] = new loc(6);
         locale[6].name = "Driveway";
-        locale[6].desc = "locale6 desc";
+        locale[6].desc = "You're in the driveway, it is extremely boring. You can walk into the street and die, but I don't think you want to do that. You should probably just got north, it's more fun.";
         locale[6].toString();
     locale[7] = new loc(7);
         locale[7].name = "Under the Car";
-        locale[7].desc = "locale7 desc";
+        locale[7].desc = "As you walk under the car, the muffler falls and hits you on the head.";
         locale[7].toString();
         mite(1);
 
@@ -61,7 +62,7 @@ function loc(id) {
     this.name = "";
     this.desc = "";
     this.toString = function() {
-        return "[Locale id=" + this.id + " name=" + this.name + " desc=" + this.desc + "]";
+        return "[Locale id=" + this.id + "] \n" + "You are in the " + this.name + "\n" + this.desc;
     }
 }
 //Item protoype
@@ -82,7 +83,7 @@ function mite(id) {
 
 function init() {
 
-    document.getElementById("taDisplay").value = "Main Garage";
+    document.getElementById("taDisplay").value = locale[0].toString();
     document.getElementById("upButton").disabled = true;
     document.getElementById("pointCounter").innerHTML='Points: ' + userPoints.points;
     //document.getElementById("roomList").style.display = 'none';
@@ -121,7 +122,7 @@ function userPoints(points) {
 function pointAdder() {
     //alert("function was called")
     if (currentLocale == 0 && timesRoomEntered.room0 < 1) {
-        userPoints.points = userPoints.points + 5;
+        //userPoints.points = userPoints.points + 5;
         timesRoomEntered.room0 = timesRoomEntered.room0 + 1;
     } else if (currentLocale == 1 && timesRoomEntered.room1 < 1) {
         userPoints.points = userPoints.points + 5;
@@ -202,7 +203,6 @@ function go(command) {
             var taPtr = document.getElementById("taDisplay");
             var history = taPtr.value;
             taPtr.value = locale[currentLocale].toString() + "\n" + history;
-            //taPtr.value = loc(currentLocale).toString + "\n" + history;
             pointAdder();
             document.getElementById("pointCounter").innerHTML='Points: ' + userPoints.points;
             
